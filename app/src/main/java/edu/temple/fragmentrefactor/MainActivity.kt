7cf4fbb98e616f122798e9af701768ff6164eeb3
Victoria <tovictoriadao@gmail.com>
2024-10-14 15:43:23 -0400

    Fill out the testFragment file

--- app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
@@ -11,18 +11,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val displayTextView = findViewById<TextView>(R.id.displayTextView)
-        val nameEditText = findViewById<EditText>(R.id.nameEditText)
-        val changeButton = findViewById<Button>(R.id.changeButton)
+//        val displayTextView = findViewById<TextView>(R.id.displayTextView)
+//        val nameEditText = findViewById<EditText>(R.id.nameEditText)
+//        val changeButton = findViewById<Button>(R.id.changeButton)
+//
+//        changeButton.setOnClickListener {
+//            val name = nameEditText.text
+//
+//            displayTextView.text = if (name.isNotBlank()) {
+//                "Hello, $name!"
+//            } else {
+//                "Please enter your name"
+//            }
+//        }
 
-        changeButton.setOnClickListener {
-            val name = nameEditText.text
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainerView, testFragment())
+            .commit()
 
-            displayTextView.text = if (name.isNotBlank()) {
-                "Hello, $name!"
-            } else {
-                "Please enter your name"
-            }
-        }
     }
 }
--- app/src/main/java/edu/temple/fragmentrefactor/testFragment.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/testFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,8 +28,21 @@ class testFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false)
+        val layout = inflater.inflate(R.layout.fragment_test, container, false)
+        val displayTextView = layout.findViewById<TextView>(R.id.displayTextView)
+        val nameEdit = layout.findViewById<TextView>(R.id.nameEditText)
+        val changeButton = layout.findViewById<Button>(R.id.changeButton).apply {
+            setOnClickListener {
+                var name = nameEdit.text
+                displayTextView.text = if (name.isNotBlank()) {
+                    "Hello, $name!"
+                } else {
+                    "Please enter your name"
+                }
+            }
+        }
 
+        return layout
     }
 
 
