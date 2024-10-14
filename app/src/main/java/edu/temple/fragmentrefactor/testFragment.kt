0d4f3daaa11b02b4145922987d13c4f58256566e
Victoria <tovictoriadao@gmail.com>
2024-10-14 15:53:04 -0400

    Clean up the code a bit

--- app/src/main/java/edu/temple/fragmentrefactor/testFragment.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/testFragment.kt
@@ -8,21 +8,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [testFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class testFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
